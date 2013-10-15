3829c09ba32793f2e3d96f2d4c50da8b2b6a7571
Gabriel Samfira <samfiragabriel@gmail.com>
2013-10-15 12:34:40 +0300

    Added license

--- jrunner/jobqueue/preprocesors/__init__.py
+++ jrunner/jobqueue/preprocesors/__init__.py
@@ -1,26 +1,31 @@
+# Copyright 2013 Cloudbase Solutions Srl
+# All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
 import importlib
 import pickle as p
+
 
 def process(task):
     """
         Unele task-uri au nevoie de procesare inante de a fi trimise catre MQ
-        Spre exemplu, in momentul adaugarii unui app cu domeniu, task-urile
-        create sunt dupa cum urmeaza:
-
-        - adaugare app in baza de date cu status "pending"
-        - adaugarea app serverelor in baza de date si adaugarea lor in task queue
-        - adaugarea balancer-ului in baza de date si trimiterea lui in MQ
-        - adaugarea APP-ului in MQ
-
-        Balancer-ul depinde de AppServer pentru a afla portul backend-ului. Drept urmare
-        informatiile din task-ul de adaugare a balancer-ului sunt incomplete pana la finalizarea
-        task-urilor anterioare.
-
         Procesatorul va adauga informatiile lipsa inainte de a face push.
     """
     procesor = None
     try:
-        procesor = importlib.import_module('jrunner.jobqueue.preprocesors.%s' % str(task.resource))
+        procesor = importlib.import_module(
+            'jrunner.jobqueue.preprocesors.%s' % str(task.resource))
     except Exception as err:
         pass
 
